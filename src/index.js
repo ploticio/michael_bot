8c9564b18314c088707425bf31840889a5edafae
darren <darrentran001@gmail.com>
2024-07-25 17:03:58 -0700

    added cron job

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+const cron = require("node-cron")
 const { Client, GatewayIntentBits } = require("discord.js")
-const { token } = require("../config.json")
+const { token, channel_id } = require("../config.json")
 
 const client = new Client({intents: [GatewayIntentBits.Guilds]})
 
-client.once("ready", (c) => {
+client.on("ready", (c) => {
     console.log("Bot is Ready");
+    const channel = client.channels.cache.get(channel_id)
+    cron.schedule("*/2 * * * * *", () => {
+        let loc_time = new Date().toLocaleTimeString([], {timeStyle: "short", timeZone: "America/Chicago"})
+        channel.send("michael time: " + loc_time)
+    })
 })
 
 client.login(token)
